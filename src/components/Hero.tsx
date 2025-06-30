e3d99cd85fc20b0cfe4a0d963e001b988d9c3753
quietdutta <anirban.dutta@tuhh.de>
2025-06-30 20:45:20 +0200

    fourth part 2 commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -164,7 +164,7 @@ const Hero = () => {
               <div className="relative transition-all duration-500 ease-out overflow-hidden rounded-2xl sm:rounded-3xl shadow-2xl">
                 <img 
                   ref={imageRef} 
-                  src="/image.png"
+                  src="img1.jpg" 
                   alt="Scrappy Robotic Arm" 
                   className="w-full h-auto object-cover transition-transform duration-500 ease-out" 
                   style={{ transformStyle: 'preserve-3d' }} 
